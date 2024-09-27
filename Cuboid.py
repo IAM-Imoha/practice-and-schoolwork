96263179102451bf29821fdd68443761c6212a75
Idris Mohammed <mohammedid@gmx.co.uk>
2024-09-27 11:58:32 +0100

    Add files via upload

---
+++ Cuboid.py
@@ -0,0 +1,5 @@
+c_width = float(input("Enter width: "))
+c_height = float(input("Enter height: "))
+c_depth = float(input("Enter depth: "))
+c_volume = c_width * c_height * c_depth
+print("Volume is", c_volume)
