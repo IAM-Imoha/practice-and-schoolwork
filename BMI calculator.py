e4e875f521314192eb51e6e82a4e820945f5b798
Idris Mohammed <mohammedid@gmx.co.uk>
2024-10-01 10:24:03 +0100

    Add files via upload

--- BMI calculator.py
+++ BMI calculator.py
@@ -1,4 +1,8 @@
 weight = int(input('Enter your weight in kg: '))
 height = int(input('Enter your height in cm: '))
-bmi = weight/(height/100)**2
-print("Your BMI is", bmi)
+#check for zero input to avoid dividing by zero
+if height == 0:
+    print('N/A')
+else:
+    bmi = weight/(height/100)**2
+    print("Your BMI is", bmi)
